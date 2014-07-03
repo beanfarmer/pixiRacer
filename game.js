c3aa9caa662f21d2ccd00b7a451247e3b6e3175e
beanfarmer <Aaronbarker27@gmail.com>
2014-07-03 14:23:45 +0100

    more comments!

--- game.js
+++ game.js
@@ -29,7 +29,10 @@ var Game = {
   shots: [],
 
   init: function(){
+    //setup keylisteners
     this.initKeys();
+
+    //setup the stage
     this.stage = new PIXI.Stage();
     this.render = PIXI.autoDetectRenderer(SCREENWIDTH, SCREENHEIGHT);
     document.body.appendChild(this.render.view);
@@ -39,12 +42,16 @@ var Game = {
     this.isMoving = false;
     this.xflip = false;
     this.yflip = false;
-    this.loadTextures();
     this.shot = false;
 
+    //get in our images
+    this.loadTextures();
+
+    //setup the background
     this.background = this.setupSprite(this.texture[0], 0.0, 0.0, 0, -260); //setup the background;
     this.stage.addChild(this.background);
 
+    //setup the player
     this.player = this.setupSprite(this.texture[1], 0.5, 0.5, PLAYERSTARTX, PLAYERSTARTY); //setup the player
     this.playerSpeedX = 0.0;
     this.playerSpeedY = 0.0;
@@ -54,16 +61,19 @@ var Game = {
   },
 
   initKeys: function(){
+
     for(var i = 0; i < 6; i++){
-      this.keys[i] = false;
-      console.log("key: " + this.keys[i]);
+      this.keys[i] = false; //set all our keys to false
+      //console.log("key: " + this.keys[i]);
     }
-    this.shot = false;
+
+    //add the keylistenters to the body
     document.body.addEventListener('keydown',Game.keyDownHandler, false);
     document.body.addEventListener('keyup',  Game.keyUpHandler, false);
   },
 
   loadTextures: function(){
+    //add our images to an array of textures
     this.texture[0] = PIXI.Texture.fromImage('img/road.png');
     this.texture[1] = PIXI.Texture.fromImage('img/car.png');
     this.texture[2] = PIXI.Texture.fromImage('img/enCar.png');
@@ -73,49 +83,64 @@ var Game = {
   },
 
   setupSprite: function(texture, anchorX, anchorY, startX, startY){
+    //create a temp sprite
     var sprite = new PIXI.Sprite(texture);
 
+    //set the anchor points for the sprite
     sprite.anchor.x = anchorX;
     sprite.anchor.y = anchorY;
 
+    //set the start pos for the sprite
     sprite.position.x = startX;
     sprite.position.y = startY;
 
+    //return our setup sprite
     return sprite;
   },
 
   boundsCheck: function(object){
+    //width first
+    //if the object is past the left of the screen
     if(object.position.x < 0){
-      object.position.x = SCREENWIDTH;
-    }else if(object.position.x > SCREENWIDTH){
+      object.position.x = SCREENWIDTH; //come out on the right side
+    }
+    else if(object.position.x > SCREENWIDTH){ //flip above
       object.position.x = 0;
     }
+
+    //then height
+    //if the player is at the top of the screen
     if(object.position.y < 0){
-      object.position.y = SCREENHEIGHT;
-    }else if(object.position.y > SCREENHEIGHT){
+      object.position.y = SCREENHEIGHT; // come out at the bottom
+    }
+    else if(object.position.y > SCREENHEIGHT){ //flip above
       object.position.y = 0;
     }
   },
 
   accelerate: function(){
+    //move up the screen
     if(this.playerSpeedY > MINSPEED){
       this.playerSpeedY -= 0.2;
     }
   },
 
   useBreaks: function(){
+    //move down the screen
     if(this.playerSpeedY < MAXSPEED){
       this.playerSpeedY += 0.2;
     }
   },
 
   moveLeft: function(){
+    //guess what this does
     if(this.playerSpeedX > MINSPEED){
       this.playerSpeedX -= 0.2;
     }
   },
 
   moveRight: function(){
+    //and this bet you cant tell
     if(this.playerSpeedX < MAXSPEED){
       this.playerSpeedX += 0.2;
     }
@@ -123,6 +148,7 @@ var Game = {
 
 
   scrollBackground: function(){
+    //loop the background
     if(this.background.position.y < 0){
         this.background.position.y += 4;
     }else{
@@ -131,24 +157,30 @@ var Game = {
   },
 
   updatePlayer: function(){
+    //move the player
     this.player.position.x += this.playerSpeedX;
     this.player.position.y += this.playerSpeedY;
   },
 
   shoot: function(){
+    //add the new spirte to the shots array
     this.shots.push(this.setupSprite(this.texture[3], 0.5, 0.0, this.player.position.x, this.player.position.y - 20));
 
+    //add that to the array
     this.stage.addChild(this.shots[this.shots.length -1]);
+
+    //set shots to false for key tracking
     this.shot = false;
   },
 
   keyDownHandler: function(event){
+    //get the key that triggered the event
     	var keyPressed = String.fromCharCode(event.keyCode);
 
        if (keyPressed == "W")
          {
-           Game.keys[0] = true;
-           isMoving = true;
+           Game.keys[0] = true; //tell our array we have pressed a key
+           isMoving = true; //tell the engine we want to be moving
          }
       else if (keyPressed == "D")
            {
@@ -165,10 +197,10 @@ var Game = {
           Game.keys[3] = true;
           isMoving = true;
         }
-      else if(keyPressed == "m")
+      else if(keyPressed == " ")
         {
-          Game.keys[4] = true;
-          this.shot = true;
+          Game.keys[4] = true; //keys[4] is shooting
+          this.shot = true; // tell the game to update the shots
         }
   },
 
@@ -195,7 +227,7 @@ var Game = {
         Game.keys[3] = false;
         isMoving = false;
       }
-    else if(keyPressed == "m")
+    else if(keyPressed == " ")
       {
         Game.keys[4] = false;
         Game.shot = false;
